6e37f17e610e880f211b15644f89db6a6a379ff9
wonhojin <ghgud0748@gmail.com>
2024-06-04 23:00:12 +0900

    24/06/06
    골드메탈 / 2D 종스크롤 슈팅 - 플레이어 이동 구현하기 / 21:25

---
+++ 240527/Assets/Vertical 2D Shooting BE4/ReadMe/Scripts/Script/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public bool isTouchTop;
+    public bool isTouchBottom;
+    public bool isTouchRight;
+    public bool isTouchLeft;
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
+
+        transform.position = curPos + nextPos;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag =="Border"){
+            switch(collision.gameObject.name){
+                case "Top":
+                isTouchTop = true;
+                break;
+            }
+        }
+    }
+}
